a91491d5b5d029171709ec0c11aefbaa52999a9e
Hashimjasem <you@example.com>
2023-01-06 01:16:31 +0800

    files now save to db

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express();
 const webRouter = require('./routes/web')
+const apiRouter = require('./routes/api')
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use(webRouter)
+app.use(apiRouter)
 
 app.listen(PORT, () => {
     console.log('app is running on http://localhost:' + PORT);
